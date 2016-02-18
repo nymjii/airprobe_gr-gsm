e4010744b8b6c1a05ead722cbdcba1327995d4d3
WarenUT <thibaut.j.garcia@gmail.com>
2016-02-18 19:08:46 +0100

    Adding a example to launch the script - temp

---
+++ launcher.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+import subprocess
+
+if __name__ == '__main__':
+    args = ["/home/badimsibox/Documents/airprobe_gr-gsm/airprobe_rtlsdr_non_graphical.py", "-n", "20"]
+    subprocess.call(args)
+
